d45d5e908bc0401bfd329811975dc51cf4aa28a9
Manish <manishginti9@gmail.com>
2025-11-09 09:51:01 +0530

    Temp rename visitor model file

--- server/controllers/visitorController.js
+++ server/controllers/visitorController.js
@@ -1,4 +1,4 @@
-import Visitor from "../models/visitorModel.js"; 
+import Visitor from "../models/_visitorModel.js"; 
 //  Create a new visitor
 export const createVisitor = async (req, res) => {
   try {
